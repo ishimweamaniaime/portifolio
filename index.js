693fe2b3d24d54bf45bb3acb21eb04c366dedc43
ishimweamaniaime <133669143+ishimweamaniaime@users.noreply.github.com>
2024-04-18 01:36:09 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var vedeo = document.querySelector("video");
+var pplay = document.querySelector("h2");
+play = document.addEventListener("click", () =>{
+if( play = document.querySelector("h2"))
+{vedeo.play}
+
+
+
+
+
+
+})
